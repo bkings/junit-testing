b162973e152be1a0eb517fb69c5d99aa3028e24e
bkings <bbikings@yahoo.com>
2022-04-08 07:55:51 +0545

    basics conclusion

--- src/test/java/com/bjr/GreetingTest.java
+++ src/test/java/com/bjr/GreetingTest.java
@@ -1,7 +1,6 @@
 package com.bjr;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,9 +14,15 @@ class GreetingTest {
 
     Greeting greeting;
 
+    @BeforeAll
+    static void classLevelSetup() {
+        System.out.println("Establishing connections and loading properties .... ");
+    }
+
     @BeforeEach
     void setUp() {
         greeting = new Greeting();
+        System.out.println("Greetings from before each");
     }
 
     @Test
@@ -28,5 +33,15 @@ class GreetingTest {
     @Test
     void testHelloWorld() {
         assertEquals("Hello Jackson", greeting.helloWorld("Jackson"));
+    }
+
+    @AfterEach
+    void cleanUp() {
+        System.out.println("Exiting after each");
+    }
+
+    @AfterAll
+    static void classLevelCleanUp() {
+        System.out.println("Closing connections");
     }
 }
