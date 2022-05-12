9993eb8b621ea9ad12238e0a597484c746516254
bkings <bbikings@yahoo.com>
2022-05-12 07:40:09 +0545

    push

--- src/test/java/com/bjr/models/PersonRepeatedTest.java
+++ src/test/java/com/bjr/models/PersonRepeatedTest.java
@@ -1,7 +1,6 @@
 package com.bjr.models;
 
 import com.bjr.interfaces.RepeatedTestInterface;
-import com.bjr.interfaces.TestInterface;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.RepetitionInfo;
