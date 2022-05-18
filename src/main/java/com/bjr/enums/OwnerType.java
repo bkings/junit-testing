10059c098217846d0df7386d16c9e83a32823653
bkings <bbikings@yahoo.com>
2022-05-18 07:56:44 +0545

    enum,csv,method source providers

---
+++ src/main/java/com/bjr/enums/OwnerType.java
@@ -0,0 +1,11 @@
+package com.bjr.enums;
+
+/**
+ * @author bkings
+ * @version 1.0.0
+ * @since 2022-05-18 07:36
+ */
+
+public enum OwnerType {
+    INDIVIDUAL, COMPANY
+}
--- src/test/java/com/bjr/models/OwnerTest.java
+++ src/test/java/com/bjr/models/OwnerTest.java
@@ -1,9 +1,15 @@
 package com.bjr.models;
 
+import com.bjr.enums.OwnerType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -32,9 +38,31 @@ class OwnerTest {
         );
     }
 
-    @ParameterizedTest
-    @ValueSource(strings = {"Test","with","parameters"})
+    @DisplayName("Value Source Test")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @ValueSource(strings = {"Test", "with", "parameters"})
     void testValueSource(String value) {
         System.out.println(value);
+    }
+
+    @DisplayName("Enum Source")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @EnumSource(value = OwnerType.class)
+    void enumTest(OwnerType ownerType) {
+        System.out.println(ownerType);
+    }
+
+    @DisplayName("From Method source")
+    @ParameterizedTest(name = "{displayName} - [{index}] {arguments}")
+    @MethodSource("argsProvider")
+    void methodSourceArgs(String prefix, String mid, int num) {
+        System.out.println("Prefix " + prefix + " all - " + mid + ":" + num);
+    }
+
+    static Stream<Arguments> argsProvider() {
+        return Stream.of(
+                Arguments.of("sth", "fishy", 1),
+                Arguments.of("nth", "here", 4),
+                Arguments.of("ok", "then", 5));
     }
 }
